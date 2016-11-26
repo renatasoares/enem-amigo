3161c588a0a06d618ad3306b33577ca1111e0de2
Pedro de Lyra <pedrodelyra@gmail.com>
2016-11-26 18:09:28 -0200

    Refactor static pages controller performance test

--- test/performance/static_pages_controller_perfomance_test.rb
+++ test/performance/static_pages_controller_perfomance_test.rb
@@ -5,23 +5,15 @@ include SessionsHelper
 
 class StaticPagePerfomanceTest < ActionDispatch::PerformanceTest
 
-	def setup
-		get '/login'
-		@user = users(:renata)
-	end
-
 	def test_help
-		log_in @user
 		get '/help'
 	end
 
 	def test_about
-		log_in @user
 		get '/about'
 	end
 
 	def test_home
-    log_in @user
 		get '/'
 	end
 
