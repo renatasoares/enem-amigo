2eab4f5790911b2f228318913ed753de0bb65bcb
Renata Soares dos Santos <renata.sds09@gmail.com>
2016-11-26 17:35:35 -0200

    Static pages perfomance test

---
+++ test/performance/static_pages_controller_perfomance_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+require 'rails/performance_test_help'
+ 
+include SessionsHelper
+
+class StaticPagePerfomanceTest < ActionDispatch::PerformanceTest
+
+	def setup
+		get '/login'
+		@user = users(:renata)
+	end
+
+	def test_help
+		log_in @user
+		get '/help'
+	end
+
+	def test_about
+		log_in @user
+		get '/about'
+	end
+
+	def test_home
+		get :home
+	end
+
+end
